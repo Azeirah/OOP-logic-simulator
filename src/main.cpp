8b316bc693268cebc6ff7442760d3fe642f38414
BrEr <brianermans@gmail.com>
2016-03-07 14:39:45 +0100

    first command is file to read

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,18 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main (int argc, char* argv[]) {
+
     Node* node = NodeFactory::get("AND");
    	cout << "I am an node and my reference is " << &node << endl;
 
-   	Reader* mijnReader = new Reader("test.txt");
+   	if (argv[1] != NULL) {
+
+   	Reader* mijnReader = new Reader(argv[1]);
    	cout << mijnReader->nextLine() << endl;
    	cout << mijnReader->nextLine() << endl;
 
+}
    //node.printInfo();
 	return -1;
 }
