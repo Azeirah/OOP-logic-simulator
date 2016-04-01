e5c8dff3eb6f5a3ca6daa6fcb7cb877b0c0b38ed
BrEr <brianermans@gmail.com>
2016-04-01 14:00:52 +0200

    Included INPUT Gates

---
+++ src/Nodes/Gates/InputHighGate.cpp
@@ -0,0 +1,25 @@
+/*
+ * InputHighGate.cpp
+ *
+ *  Created on: Mar 1, 2016
+ *      Author: brian
+ */
+#include "InputHighGate.h"
+#include <iostream>
+using namespace std;
+
+InputHighGate InputHighGate::instance("INPUT_HIGH");
+
+InputHighGate::InputHighGate( const char* szArg ) : LogicGate(szArg) {
+}
+
+InputHighGate::InputHighGate() {
+}
+
+InputHighGate::~InputHighGate() {
+	// TODO Auto-generated destructor stub
+}
+
+Node* InputHighGate::clone() {
+    return new InputHighGate();
+}
---
+++ src/Nodes/Gates/ProbeGate.cpp
@@ -0,0 +1,25 @@
+/*
+ * ProbeGate.cpp
+ *
+ *  Created on: Mar 1, 2016
+ *      Author: brian
+ */
+#include "ProbeGate.h"
+#include <iostream>
+using namespace std;
+
+ProbeGate ProbeGate::instance("PROBE");
+
+ProbeGate::ProbeGate( const char* szArg ) : LogicGate(szArg) {
+}
+
+ProbeGate::ProbeGate() {
+}
+
+ProbeGate::~ProbeGate() {
+	// TODO Auto-generated destructor stub
+}
+
+Node* ProbeGate::clone() {
+    return new ProbeGate();
+}
